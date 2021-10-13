1d48cd040b9c983a223bc8f9e872a190bc28f73b
unknown <osamagul08@gmail.com>
2021-10-13 03:43:51 -0700

    second commit

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set('views', path.join(__dirname, 'views'))
 if (process.env.NODE_ENV === "dev") {
     app.use(morgan('dev'))
 }
-
+console.log('osama')
 //access static file like overvieww.html
 app.use(express.static( path.join(__dirname, 'public')))
 
